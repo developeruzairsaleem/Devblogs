7d2529fe6e98e445c9e15ad31de8438a4397066b
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-26 16:45:06 +0500

    Final for production

--- src/api/internal.js
+++ src/api/internal.js
@@ -1,6 +1,7 @@
+
 import axios from "axios";
 const api = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:process.env.BACKEND_API_PATH,
     headers:{
         "Content-Type":"multipart/form-data"
     }
