c961d4c5d33e7e4870484701dbbe2a70b444464e
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-26 17:11:28 +0500

    error fixed

--- src/api/internal.js
+++ src/api/internal.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const api = axios.create({
-    baseURL:process.env.BACKEND_API_PATH,
+    baseURL:'https://blog-application-by-uzair.onrender.com',
     headers:{
         "Content-Type":"multipart/form-data"
     }
